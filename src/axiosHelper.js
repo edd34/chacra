7fb24a7b3416842e783f97265c9e3bbe0909cdf7
Eddine Omar <eddine.farid@hotmail.fr>
2020-09-05 15:41:20 +0200

    update cors

--- src/axiosHelper.js
+++ src/axiosHelper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: "https://intense-castle-64808.herokuapp.com/",
     headers: {
         "Content-type": "application/json",
         "Access-Control-Allow-Origin": "true"
